d4c88062c1500f9b3ef53b0f944d61e45ed0785c
ITSC <ITSC@test.com>
2023-04-23 14:54:18 +0900

    서식문자

--- src/section02/ConsolePrint.java
+++ src/section02/ConsolePrint.java
@@ -5,6 +5,16 @@ package section02;
  * System.out.print() : 괄호안 내용 출력 행 바꿈x
  * System.out.println() : 괄호안 내용 출력 후 행 바꿈
  * System.out.printf() : 문자열을 서식 문자를 이용해 형식화된 내용 출력 행 바꿈x
+ * 
+ * 서식문자
+ * 		%d : 정수(10진수)
+ * 		%o : 정수(8진수)
+ * 		%x : 정수(16진수)
+ * 		#f : 실수
+ * 		%e : 지수(e표기 기반)
+ * 		%g : 출력 대상에 따라 %e 또는 %f 형태로 출력
+ * 		%s : 문자열
+ * 		%c : 문자
  */
 
 public class ConsolePrint {
@@ -12,7 +22,8 @@ public class ConsolePrint {
 	public static void main(String[] args) {
 		System.out.print("Welcome.");
 		System.out.println("Java World");
-		System.out.printf("오늘은 %d월 %d일 입니다.", 4, 22);
+		System.out.printf("오늘은 %d월 %d일 입니다.\n", 4, 22);
+		System.out.printf("%d은 첫번째, %f은 두번째, %s은 세번째." , 1, 2.0, "셋");
 	}
 
 }
