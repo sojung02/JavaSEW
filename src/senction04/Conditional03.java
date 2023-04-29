fdf8adb16c2247a7109dc632731079fb9ebea2ec
ITSC <ITSC@test.com>
2023-04-29 17:17:12 +0900

    wasd case문

---
+++ src/senction04/Conditional03.java
@@ -0,0 +1,25 @@
+package senction04;
+
+public class Conditional03 {
+	public static void main(String[] args) {
+		char key = 'w';
+		switch(key) {
+		case 'w':
+			System.out.println("앞으로 이동");
+			break;
+		case 'a':
+			System.out.println("좌측으로 이동");
+			break;
+		case 's':
+			System.out.println("뒤로 이동");
+			break;
+		case 'd':
+			System.out.println("우측으로 이동");
+			break;
+		default :
+			System.out.println("Hold!");
+			break;
+		}
+	}
+
+}
