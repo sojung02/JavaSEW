bb905db3dcc702e7c47ffb36c47f2ddf93dba0e3
ITSC <ITSC@test.com>
2023-04-29 15:52:42 +0900

    비트연산자

---
+++ src/section03/Operator06.java
@@ -0,0 +1,22 @@
+package section03;
+/*
+ * 6. 비트 연산자
+ * 	2진수 표현된 두 비트 연산자
+ * 
+ * 	& : 대응 되는 비트가 모두 1이면 1을 반환 (비트 AND 연산자)
+ *  | : 대응 되는 비트 중에서 하나라도 1이면 1을 반환 (비트 OR 연산자)
+ *  ^ : 대응 되는 비트가 서로 다르면 1을 반환 * (비트 XOR 연산자)
+ *  ~ : 비트가 1이면 0으로, 0이면 1로 반전시킨 (비트 NOT 연산자)
+ */
+
+public class Operator06 {
+	public static void main(String[] args) {
+		int num1 = 3; // 0 0011
+		int num2 = 5; // 0 0101
+		
+		System.out.println("num1 & num2: " + (num1 & num2));
+		System.out.println("num1 | num2: " + (num1 | num2));
+		System.out.println("num1 ^ num2: " + (num1 ^ num2));
+		System.out.println("~num1: " + ~num1);
+	}
+}
